53d8ba50fc5dcaf97ca04675df2fd5d878af5686
hoppe <hwa125@sfu.ca>
2018-12-05 16:15:03 -0800

    print out message to test

--- model_train.py
+++ model_train.py
@@ -17,24 +17,24 @@ from pyspark.mllib.stat import Statistics
 import pandas as pd
 import numpy as np
 
-from ETL import ETL, get_dummies_spark
+from ETL import get_dummies_spark
 
-def main():
+def model_training():
 
     ###################################
     # after we read in the ready_train and ready_test, we first on-hot-encode the categorical data
-    #ready_train, ready_test = ETL()
-    ready_train = spark.read.option("inferSchema", True).csv('/Users/michaelyang/Downloads/all_data/application_train.csv',
-                                   header=True)
-    train_cat_encoded = get_dummies_spark(ready_train,'SK_ID_CURR','ready_train')
+    ready_train = spark.read.option("inferSchema", True).csv('ready_train/',header=True)
+    train_cat_encoded = get_dummies_spark(ready_train,'sk_id_curr','ready_train')
     numerical_feats = [f for f, t in ready_train.dtypes if t != 'string']
     train_num_df = ready_train.select(numerical_feats)
-    ready_train = train_cat_encoded.join(train_num_df,on = 'SK_ID_CURR')
+    ready_train = train_cat_encoded.join(train_num_df,on = 'sk_id_curr')
+    print('************finished one-hot-encoding the training set******************')
+    print('*************total number of features we have***********************',len(ready_train.columns))
 
-    # test_cat_encoded = get_dummies_spark(ready_test, 'SK_ID_CURR', 'ready_test')
+    # test_cat_encoded = get_dummies_spark(ready_test, 'sk_id_curr', 'ready_test')
     # numerical_feats = [f for f, t in ready_test.dtypes if t != 'string']
     # test_num_df = ready_test.select(numerical_feats)
-    # ready_test = test_cat_encoded.join(test_num_df, on='SK_ID_CURR')
+    # ready_test = test_cat_encoded.join(test_num_df, on='sk_id_curr')
 
     #####################################
     # feature generation
@@ -42,11 +42,11 @@ def main():
     CREDIT_INCOME_PERCENT: the percentage of the credit amount relative to a client's income
     ANNUITY_INCOME_PERCENT: the percentage of the loan annuity relative to a client's income
     CREDIT_TERM:  the length of the payment in months (since the annuity is the monthly amount due
-    DAYS_EMPLOYED_PERCENT: the percentage of the days employed relative to the client's age
     '''
-    ready_train = ready_train.withColumn('CREDIT_INCOME_PERCENT', ready_train.AMT_CREDIT / ready_train.AMT_INCOME_TOTAL)
-    ready_train = ready_train.withColumn('ANNUITY_INCOME_PERCENT', ready_train.AMT_ANNUITY/ ready_train.AMT_INCOME_TOTAL)
-    ready_train = ready_train.withColumn('CREDIT_TERM', ready_train.AMT_ANNUITY/ ready_train.AMT_CREDIT)
+    ready_train = ready_train.withColumn('credit_income_percent', ready_train.amt_credit / ready_train.amt_income_total)
+    ready_train = ready_train.withColumn('annuity_income_percent', ready_train.amt_annuity/ ready_train.amt_income_total)
+    ready_train = ready_train.withColumn('credit_term', ready_train.amt_annuity/ ready_train.amt_credit)
+    print('**************finished generating features from domain knowledge*************')
 
     ######################################
     # remove invalid outliers
@@ -61,12 +61,12 @@ def main():
     # tree models are insensitive to redundant features, but feature selection is still necessary.
     '''
     by combining features of all tables together, we end up with 1014 features. Some of the features might be collinear
-    to each other. These can decrease the model's availablility to learn, decrease model interpretability, and decrease
+    to each other. These can decrease the model's availability to learn, decrease model interpretability, and decrease
      generalization performance on the test set. We want to remove these features.
     '''
     # Threshold for removing correlated variables = 0.9
     threshold = 0.9
-    df = ready_train.drop('SK_ID_CURR').drop('TARGET')
+    df = ready_train.drop('sk_id_curr').drop('target')
     col_names = df.columns
     features = df.rdd.map(lambda row: row[0:])
     corr_mat = Statistics.corr(features, method="pearson")
@@ -74,6 +74,7 @@ def main():
     corr_df.index, corr_df.columns = col_names, col_names
     upper = corr_df.where(np.triu(np.ones(corr_df.shape), k=1).astype(np.bool))
     to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
+    print('******remove collinear features*********',to_drop)
 
     # drop columns in the to_drop list
     for col in ready_train.columns:
@@ -81,20 +82,24 @@ def main():
             ready_train.drop(col)
             #ready_test.drop(col)
 
+
     #########################################
     # remove columns with over 75% of missing values
-    nullcount = ready_train.select([count(when(isnull(c), c)).alias(c) for c in ready_train.columns]).collect()
+    null_count = ready_train.select([count(when(isnull(c), c)).alias(c) for c in ready_train.columns]).collect()
     size = ready_train.count()
     threshold = 0.75 * size
     to_drop = []
     for col in ready_train.columns:
-        if nullcount[0][col] > threshold:
+        if null_count[0][col] > threshold:
             to_drop.append(col)
+    print('********remove columns with 75% of missing value************',to_drop)
 
     for col in ready_train.columns:
         if col in to_drop:
             ready_train.drop(col)
             #ready_test.drop(col)
+
+    print('*********number of features left after removing collinear and missing columns*************',len(ready_train.columns))
 
     #########################################
     '''
@@ -105,10 +110,10 @@ def main():
     '''
     # feature importance, choose from model
     # assembler
-    features = list(set(ready_train.columns) - set(['SK_ID_CURR', 'TARGET']))
+    features = list(set(ready_train.columns) - set(['sk_id_curr', 'target']))
     feature_assembler = VectorAssembler(inputCols=features, outputCol='features')
     # classifier
-    classifier = RandomForestClassifier(labelCol='TARGET', maxBins=60, maxDepth=8)
+    classifier = RandomForestClassifier(labelCol='target', maxBins=60, maxDepth=8)
     # pipeline
     credit_pipeline = Pipeline(stages=[feature_assembler, classifier])
     # fit and see feature importance
@@ -117,24 +122,36 @@ def main():
     feature_importances = pd.DataFrame({'feature': features, 'importance': credit_model.stages[-1].featureImportances})\
         .sort_values('importance', ascending=False)
     zero_features = list(feature_importances[feature_importances['importance'] == 0.0]['feature'])
+    print('*********remove features with zero importance***************',zero_features)
 
     for col in ready_train.columns:
         if col in zero_features:
             ready_train.drop(col)
             #ready_test.drop(col)
 
+    print('************features left after model selection*****************',ready_train.columns)
+    print('************number of features left************', len(ready_train.columns))
+    print('************get best model through cross validation*************')
+
     #############################################
     # use cross validation to tune hyperparameter
     # use area under ROC instead of accuracy since we have unbalanced datasets (default metric for binaryclassificationevaluator)
-    features = list(set(ready_train.columns) - set(['SK_ID_CURR', 'TARGET']))
+    features = list(set(ready_train.columns) - set(['sk_id_curr', 'target']))
     feature_assembler = VectorAssembler(inputCols=features, outputCol='features')
-    classifier = GBTClassifier(labelCol='TARGET', maxBins=60)
+    classifier = GBTClassifier(labelCol='target', maxBins=60)
     pipeline = Pipeline(stages=[feature_assembler, classifier])
     grid = ParamGridBuilder().addGrid(classifier.maxDepth,[5,6,7]).addGrid(classifier.stepSize,[0.05,0.1]).build()
-    evaluator = BinaryClassificationEvaluator(labelCol='TARGET')
+    evaluator = BinaryClassificationEvaluator(labelCol='target')
     cv = CrossValidator(estimator=pipeline, estimatorParamMaps=grid, evaluator=evaluator, numFolds= 5)
-    cvModel = cv.fit(ready_train.fillna(-999))
+    cv_model = cv.fit(ready_train.fillna(-999))
+    model = cv_model.bestModel
+    # print best model hyperparameters
+    print(model.stages[1].extractParamMap())
+
+    return model
 
 if __name__ == '__main__':
 
-    pass
+    out_dir = sys.argv[1]
+    home_credit_model = model_training()
+    home_credit_model.save(out_dir)
